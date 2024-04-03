c8a5beddcf0476a8135dce33ac888fd6fa00d793
skybondm <150842432+skybondm@users.noreply.github.com>
2024-04-03 16:54:00 +0300

    Add files via upload

--- pairs.js
+++ pairs.js
@@ -1,13 +1,17 @@
-const board = document.querySelector('.settings')
+const board = document.querySelector('.board')
 const input = document.querySelector('.settings_input')
 const button = document.querySelector('.start')
 
-function clearBoard(columns) {
+function clearBoard(count, columns) {
     board.textContent = ''
-    const template = document.querySelector('#gameTableTemplate')
+    const template = document.querySelector('#gameTableTemplate').cloneNode(true).content
     const table = template.querySelector('.table')
     const restart = template.querySelector('.table_button')
-    // alert(columns)
+    const icons = createIconsArray(count)
+    icons.forEach (function (icon){
+        table.append(creatCard(icon))
+    })
+        
     table.style = `
     grid-template-columns: repeat(${columns}, 1fr);
     grid-template-rows: repeat(${columns}, 1fr);
@@ -24,5 +28,15 @@ button.addEventListener('click', function(event){
     let columns = input.value
     
     let count = columns*columns
-    clearBoard(columns)
+    clearBoard(count, columns)
 })
+function creatCard(flippedIcon){
+    const template = document.querySelector('#card').cloneNode(true).content
+    const card = template.querySelector('.cards')
+    card.querySelector('#flippedIcon').classList.add(`fa-${flippedIcon}`)
+    return card
+}
+function createIconsArray(initialCount){
+    const cardsIcons = ["compass", "cloud", "play", "bolt"]
+    return cardsIcons
+}
